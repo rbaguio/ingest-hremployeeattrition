f564d123ecd2e46f486858eccd046e7960a0dedb
Nico Baguio <rey.baguio@dataseer.com>
2018-03-15 12:54:07 +0800

    initialize employee class creation per row in dataframe

--- scripts/generate_data.py
+++ scripts/generate_data.py
@@ -1,7 +1,18 @@
 from util.regression_promotion_days import lamb
 from util.initialize_data import e_records_df, promotion_date_list
+from util.models import Employee
 from datetime import timedelta
 import numpy as np
+
+np.random.seed(2000)
+
+
+def create_employee_class(idx):
+    return Employee(
+        e_records_df.drop(
+            ['promotion_date', 'hiring_date', 'termination_date'],
+            axis=1).iloc[idx].to_dict()
+    )
 
 
 def hiring_promotion_diff(promotion_date, hiring_date):
